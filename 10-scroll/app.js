d0da7b96f4812589fc16c77d369c47f9d5fd7c18
ogzhn <oguzhancavdar06@gmail.com>
2021-11-08 23:15:15 +0300

    add back to top btn

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -35,6 +35,11 @@ window.addEventListener('scroll', function(){
   }else{
     navbar.classList.remove('fixed-nav')
   }
+  if(scrollHeight > 500){
+    topLink.classList.add('show-link')
+  }else{
+    topLink.classList.remove('show-link')
+  }
 })
 // ********** smooth scroll ************
 // select links
