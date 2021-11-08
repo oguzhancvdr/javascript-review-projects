5fc86e5348f365867da6680140187de20508fb43
ogzhn <oguzhancavdar06@gmail.com>
2021-11-08 22:46:42 +0300

    fix navbar when navbar height exceeded

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -1,7 +1,7 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
+// ? Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// ? pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// ? slice extracts a section of a string without modifying original string
+// ? offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
 const date = new Date()
@@ -14,10 +14,27 @@ const linksContainer = document.querySelector('.links-container')
 const links = document.querySelector('.links')
 
 navToggle.addEventListener('click', function(){
-  linksContainer.classList.toggle('show-links')
+  // linksContainer.classList.toggle('show-links')
+  const containerHeight = linksContainer.getBoundingClientRect().height
+  const linksHeight = links.getBoundingClientRect().height
+  if(containerHeight === 0){
+    linksContainer.style.height = `${linksHeight}px`
+  }else{
+    linksContainer.style.height = 0
+  }
 })
 
+const navbar = document.getElementById('nav')
+const topLink = document.querySelector('.top-link')
 // ********** fixed navbar ************
-
+window.addEventListener('scroll', function(){
+  const scrollHeight = window.pageYOffset
+  const navbarHeight = navbar.getBoundingClientRect().height
+  if(scrollHeight > navbarHeight){
+    navbar.classList.add('fixed-nav')
+  }else{
+    navbar.classList.remove('fixed-nav')
+  }
+})
 // ********** smooth scroll ************
 // select links
