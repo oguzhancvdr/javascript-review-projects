55e5582c0effd2e2396fd121f32f76b02e1fc7b0
ogzhn <oguzhancavdar06@gmail.com>
2021-11-08 22:27:45 +0300

    set date dynamically

---
+++ 10-scroll/app.js
@@ -0,0 +1,23 @@
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
+
+// ********** set date ************
+const date = new Date()
+const year = date.getFullYear()
+const dateEl = document.getElementById('date').textContent = year;
+
+// ********** close links ************
+const navToggle = document.querySelector('.nav-toggle')
+const linksContainer = document.querySelector('.links-container')
+const links = document.querySelector('.links')
+
+navToggle.addEventListener('click', function(){
+  linksContainer.classList.toggle('show-links')
+})
+
+// ********** fixed navbar ************
+
+// ********** smooth scroll ************
+// select links
