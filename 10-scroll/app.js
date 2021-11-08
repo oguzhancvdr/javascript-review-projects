c2370c15bec2776683e1c36575beca4e8f70dd2d
ogzhn <oguzhancavdar06@gmail.com>
2021-11-08 23:16:23 +0300

    calc height of navbar and provide that user can navigate right section

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -6,20 +6,20 @@
 // ********** set date ************
 const date = new Date()
 const year = date.getFullYear()
-const dateEl = document.getElementById('date').textContent = year;
+const dateEl = (document.getElementById('date').textContent = year)
 
 // ********** close links ************
 const navToggle = document.querySelector('.nav-toggle')
 const linksContainer = document.querySelector('.links-container')
 const links = document.querySelector('.links')
 
-navToggle.addEventListener('click', function(){
+navToggle.addEventListener('click', function () {
   // linksContainer.classList.toggle('show-links')
   const containerHeight = linksContainer.getBoundingClientRect().height
   const linksHeight = links.getBoundingClientRect().height
-  if(containerHeight === 0){
+  if (containerHeight === 0) {
     linksContainer.style.height = `${linksHeight}px`
-  }else{
+  } else {
     linksContainer.style.height = 0
   }
 })
@@ -27,19 +27,48 @@ navToggle.addEventListener('click', function(){
 const navbar = document.getElementById('nav')
 const topLink = document.querySelector('.top-link')
 // ********** fixed navbar ************
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
   const scrollHeight = window.pageYOffset
   const navbarHeight = navbar.getBoundingClientRect().height
-  if(scrollHeight > navbarHeight){
+  if (scrollHeight > navbarHeight) {
     navbar.classList.add('fixed-nav')
-  }else{
+  } else {
     navbar.classList.remove('fixed-nav')
   }
-  if(scrollHeight > 500){
+  if (scrollHeight > 500) {
     topLink.classList.add('show-link')
-  }else{
+  } else {
     topLink.classList.remove('show-link')
   }
 })
+
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link')
+
+scrollLinks.forEach(function(link){
+  link.addEventListener('click', function(e){
+    // prevent default
+    e.preventDefault()
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute('href').slice(1) // skip #
+    const element = document.getElementById(id)
+    // calculate the heights
+    const navHeight = navbar.getBoundingClientRect().height
+    const containerHeight = linksContainer.getBoundingClientRect().height
+    const fixedNav = navbar.classList.contains('fixed-nav')
+  
+    let position = element.offsetTop - navHeight
+    if(!fixedNav){
+      position = position - navHeight
+    }
+    if(navHeight > 82){
+      position = position + containerHeight
+    }
+    window.scrollTo({
+      left:0,
+      top:position,
+    })
+    linksContainer.style.height = 0;
+  })
+})
