1308721cd5c1a40ec8ea557625b903e7dac08125
ogzhn <oguzhancavdar06@gmail.com>
2021-11-13 20:20:51 +0300

    select elements

---
+++ 14-grocerybud/app.js
@@ -0,0 +1,21 @@
+// ****** SELECT ITEMS **********
+const alert = document.querySelector('.alert')
+const form = document.querySelector('.grocery-form')
+const grocery = document.getElementById('grocery')
+const submitBtn = document.querySelector('.submit-btn')
+const container = document.querySelector('.grocery-container')
+const list = document.querySelector('.grocery-list')
+const clearBttn = document.querySelector('.clear-btn')
+
+// edit option
+let editElement
+let editFlag = false
+let editID = ''
+
+// ****** EVENT LISTENERS **********
+
+// ****** FUNCTIONS **********
+
+// ****** LOCAL STORAGE **********
+
+// ****** SETUP ITEMS **********
