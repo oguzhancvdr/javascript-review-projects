dfda56b9ee1d9bd20e45876a29dba370cfc419ce
ogzhncvdr <oguzhancavdar06@gmail.com>
2022-02-27 00:08:26 +0300

    create counter logic with proto

---
+++ 16-counter-oop/app-proto.js
@@ -0,0 +1,46 @@
+function getElement(selection) {
+    const element = document.querySelector(selection);
+    if (element) {
+      return element;
+    }
+    throw new Error(
+      `Please check "${selection}" selector, no such element exists`
+    );
+  }
+  
+  function Counter(element, value) {
+    this.counter = element;
+    this.value = value;
+    this.resetBtn = element.querySelector(".reset");
+    this.decreaseBtn = element.querySelector(".decrease");
+    this.increaseBtn = element.querySelector(".increase");
+    this.valueDOM = element.querySelector(".value");
+    this.valueDOM.textContent = this.value;
+    // bind this to all function
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this);
+    this.reset = this.reset.bind(this);
+    // if we didn't bind, this.increase will point button element
+    // bu when we bind with this, it points Counter.
+    // so we can now use Counter's prototype's methods like increase , decrease
+    this.increaseBtn.addEventListener("click", this.increase);
+    this.decreaseBtn.addEventListener("click", this.decrease);
+    this.resetBtn.addEventListener("click", this.reset);
+  }
+  
+  Counter.prototype.increase = function () {
+    this.value++;
+    this.valueDOM.textContent = this.value;
+  };
+  Counter.prototype.decrease = function () {
+    this.value--;
+    this.valueDOM.textContent = this.value;
+  };
+  Counter.prototype.reset = function () {
+    this.value = 0;
+    this.valueDOM.textContent = this.value;
+  };
+  
+  const firstCounter = new Counter(getElement(".first-counter"), 0);
+  const secondCounter = new Counter(getElement(".second-counter"), 0);
+  
