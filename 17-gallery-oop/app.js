394e037d2771e6e69a599bc575a08fa43e9cd0ff
ogzhncvdr <oguzhancavdar06@gmail.com>
2022-03-07 14:45:51 +0300

    select elements

--- 17-gallery-oop/app.js
+++ 17-gallery-oop/app.js
@@ -7,3 +7,20 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+function Gallery(element) {
+  // take all imgs
+  // this will procude NodeList
+  // with spread operator we can convert to array
+  this.list = [...element.querySelectorAll('.img')]
+  // target
+  this.modal = getElement('.modal')
+  this.modalImg = getElement('.main-img')
+  this.modalImages = getElement('.modal-images')
+  this.closeBtn = getElement('.close-btn')
+  this.nextBtn = getElement('.next-btn')
+  this.prevBtn = getElement('.prev-btn')
+}
+
+const nature = new Gallery(getElement(".nature"));
+const city = new Gallery(getElement(".city"));
