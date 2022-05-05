07767884aa3ec3f4de64bd018f10243f0175c546
oguzhancvdr <oguzhancavdar06@gmail.com>
2022-05-05 18:01:14 +0300

    add leaflet library

--- 20-mapfy-oop/script.js
+++ 20-mapfy-oop/script.js
@@ -1,6 +1,19 @@
 "use strict";
 
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 const form = document.querySelector(".form");
 const containerWorkouts = document.querySelector(".workouts");
@@ -9,3 +22,36 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+const optionsOfGeolocation = {
+  enableHighAccuracy: true,
+  timeout: 3000, // Amount of time before the error callback is invoked
+  maximumAge: 0, // Maximum cached position age in miliseconds
+};
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      // L.map('map') -> string map should point our html element with id=name
+      // map will be inserted into that element
+      const coords = [latitude, longitude]
+      const map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
+    },
+    function () {
+      alert("Could not get your location");
+    },
+    optionsOfGeolocation
+  );
+}
