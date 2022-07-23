2bfa0c3111e1bdb9504843c5829da6085e83695a
ogzhncvdr <oguzhancavdar06@gmail.com>
2022-07-23 16:24:44 +0300

    add success message when user uploaded recipe

--- 22-forkify/src/js/views/addRecipeView.js
+++ 22-forkify/src/js/views/addRecipeView.js
@@ -3,6 +3,7 @@ import View from './View'
 
 class AddRecipeView extends View{
   _parentElement = document.querySelector('.upload')
+  _message = 'Recipe was successfully uploaded!'
   _window = document.querySelector('.add-recipe-window')
   _overlay = document.querySelector('.overlay')
   _btnOpen = document.querySelector('.nav__btn--add-recipe')
