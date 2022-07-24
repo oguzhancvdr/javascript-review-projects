536cec54b8602e9d74065485d11d6b84c354cdf4
ogzhncvdr <oguzhancavdar06@gmail.com>
2022-07-24 11:58:41 +0300

    add cos string and todos

--- 22-forkify/src/js/views/View.js
+++ 22-forkify/src/js/views/View.js
@@ -3,6 +3,15 @@ import icons from 'url:../../img/icons.svg'; // PARCEL v2
 export default class View{
   _data;
 
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author Oguzhan Cavdar 
+   * @todo Finish implementation
+   */
   render(data, render=true){
     if(!data || (Array.isArray(data) && data.length === 0)) return this.renderError();
 
